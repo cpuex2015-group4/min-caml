359ab96fe1616dcaa38ecfbf8b9aff77e90287b7
levelfour <levelfourslv@gmail.com>
2015-10-02 09:33:55 +0900

    add debug spec

---
+++ debug.ml
@@ -0,0 +1,5 @@
+let parse e =
+  print_string "debug parser"
+
+let knormal e =
+  print_string "debug kNormalization"
--- main.ml
+++ main.ml
@@ -21,13 +21,30 @@ let lexbuf outchan l = (* バッファをコンパイルしてチャンネルへ出力する (caml2htm
 			 (Typing.f
 			    (Parser.exp Lexer.token l)))))))))
 
+let debug_parser outchan l = (* パーサの結果をデバッグ出力する (caml2html: debug_parser) *)
+  Id.counter := 0;
+  Typing.extenv := M.empty;
+	(Debug.parse
+		(Parser.exp Lexer.token l))
+
+let debug_knormal outchan l = (* K正規化の結果をデバッグ出力する (caml2html: debug_knormal) *)
+  Id.counter := 0;
+  Typing.extenv := M.empty;
+	(Debug.knormal
+		 (KNormal.f
+			 (Typing.f
+			    (Parser.exp Lexer.token l))))
+
 let string s = lexbuf stdout (Lexing.from_string s) (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
+
+(* 対象ファイルに適用する関数(デフォルトはコンパイル関数lexbuf) *)
+let spec = ref lexbuf
 
 let file f = (* ファイルをコンパイルしてファイルに出力する (caml2html: main_file) *)
   let inchan = open_in (f ^ ".ml") in
   let outchan = open_out (f ^ ".s") in
   try
-    lexbuf outchan (Lexing.from_channel inchan);
+    !spec outchan (Lexing.from_channel inchan);
     close_in inchan;
     close_out outchan;
   with e -> (close_in inchan; close_out outchan; raise e)
@@ -36,7 +53,9 @@ let () = (* ここからコンパイラの実行が開始される (caml2html: main_entry) *)
   let files = ref [] in
   Arg.parse
     [("-inline", Arg.Int(fun i -> Inline.threshold := i), "maximum size of functions inlined");
-     ("-iter", Arg.Int(fun i -> limit := i), "maximum number of optimizations iterated")]
+     ("-iter", Arg.Int(fun i -> limit := i), "maximum number of optimizations iterated");
+     ("-parse", Arg.Unit(fun () -> spec := debug_parser), "debug print for the result of parser");
+     ("-knormal", Arg.Unit(fun () -> spec := debug_knormal), "debug print for the result of kNormalization")]
     (fun s -> files := !files @ [s])
     ("Mitou Min-Caml Compiler (C) Eijiro Sumii\n" ^
      Printf.sprintf "usage: %s [-inline m] [-iter n] ...filenames without \".ml\"..." Sys.argv.(0));
